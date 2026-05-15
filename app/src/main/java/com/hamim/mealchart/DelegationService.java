2f7cc0ed8560dc091bee51e07ab9eb985db5a561
hamimLohani <hamimlohani@gmail.com>
2026-05-15 08:43:35 +0600

    Rebuild app

---
+++ app/src/main/java/com/hamim/mealchart/DelegationService.java
@@ -0,0 +1,14 @@
+package com.hamim.mealchart;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
