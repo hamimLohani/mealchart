b15db308d3e6b0b87b7857aaf98d758debd288e9
hamimLohani <hamimlohani@gmail.com>
2026-05-15 08:25:28 +0600

    fix assetlinks 404 with nextjs rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/assetlinks.json",
+        destination: "/assetlinks.json",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
